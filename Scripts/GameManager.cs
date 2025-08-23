b19b9471ba1f1e119195c8de5bdb8e294c2c6003
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-08-23 20:55:04 +0600

    setting up loading screen

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public int selectedCarIndex = 0; // the choosen car
+
+    public static string sceneToLoad;
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    public static void LoadScence(string scenceName)
+    {
+        sceneToLoad = scenceName;
+        SceneManager.LoadScene("LoaderScene");
+    }
+}
